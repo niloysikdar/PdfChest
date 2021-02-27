2f68b6b313db50070da224055b9e9d39932946a8
Niloy Sikdar <niloysikdar30@gmail.com>
2021-02-27 23:28:51 +0530

    Fetched json response and displaying the title

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,7 +8,22 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  TextEditingController querycontroller;
+  final querycontroller = TextEditingController();
+  int _itemcount = 0;
+  var jsonResponse;
+
+  Future<void> getJsondata(String query) async {
+    String url = "https://bookapi.pythonanywhere.com/api/" + query.trim();
+    http.Response response = await http.get(url);
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+      _itemcount = jsonResponse.length;
+      setState(() {});
+    } else {
+      print("error is happening");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,42 +35,46 @@ class _HomeScreenState extends State<HomeScreen> {
         color: Colors.white,
         child: Stack(
           children: [
-            Container(
-              margin: EdgeInsets.only(top: 80.0),
-              child: ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.symmetric(
-                      horizontal: 20.0,
-                      vertical: 10.0,
-                    ),
-                    padding: EdgeInsets.all(20.0),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "hello sdjhdsjhfdjklfv fd jhbfd blfdibdifjbdf fddfnifdnfdj",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
+            (_itemcount != 0)
+                ? Container(
+                    margin: EdgeInsets.only(top: 80.0),
+                    child: ListView.builder(
+                      itemCount: _itemcount,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: EdgeInsets.symmetric(
+                            horizontal: 20.0,
+                            vertical: 10.0,
                           ),
-                        ),
-                        SizedBox(height: 10.0),
-                        Container(
-                          height: 40.0,
-                          width: 100.0,
-                          color: Colors.white,
-                        ),
-                      ],
+                          padding: EdgeInsets.all(20.0),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                jsonResponse[index]["title"],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                ),
+                              ),
+                              SizedBox(height: 10.0),
+                              Container(
+                                height: 40.0,
+                                width: 100.0,
+                                color: Colors.white,
+                              ),
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            ),
+                  )
+                : Center(
+                    child: Text("Searching"),
+                  ),
             Container(
               alignment: Alignment.topCenter,
               child: Container(
@@ -84,7 +105,19 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                       ),
                     ),
-                    Container(
+                    GestureDetector(
+                      onTap: () {
+                        String query = querycontroller.text;
+                        querycontroller.text = "";
+                        setState(() {
+                          _itemcount = 0;
+                        });
+                        getJsondata(query).then(
+                          (value) => {
+                            print("Completed"),
+                          },
+                        );
+                      },
                       child: Container(
                         padding: EdgeInsets.only(left: 8.0),
                         child: Icon(
